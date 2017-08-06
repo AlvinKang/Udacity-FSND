21b7415d02d18fd02b25fecef3b6fe9bd73e2cd9
Alvin Kang <oneinallin1@gmail.com>
2017-08-05 20:10:48 -0700

    Changed properties of the turtle

--- square_draw.py
+++ square_draw.py
@@ -5,6 +5,10 @@ def draw_square():
     window.bgcolor("red")
 
     squareArtist = turtle.Turtle()
+    squareArtist.speed(3)
+    squareArtist.color("blue")
+    squareArtist.shape("classic")
+    
     for i in range(4):
         squareArtist.forward(100)
         squareArtist.right(90)
