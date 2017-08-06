60375c1d1200cbae9f298bcab968f9ebbe523a20
Alvin Kang <oneinallin1@gmail.com>
2017-08-05 19:16:07 -0700

    Add comment

--- break_time.py
+++ break_time.py
@@ -2,7 +2,8 @@ import webbrowser
 import time
 print("This program started on: " + str(time.ctime()) + "\n")
 for i in range(0,3):
-    time.sleep(5)
+    # Every two hours
+    time.sleep(2*60*60)
     webbrowser.open("https://youtu.be/jCHriYAr6Qw")
     j = i+1
     print("Loop count: " + str(j) + "\nCurrent time: " + str(time.ctime()))
