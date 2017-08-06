91e92c5be0fdd9c6d895ecd02be1c9a1155bb818
Alvin Kang <oneinallin1@gmail.com>
2017-08-05 20:05:48 -0700

    Add draw_square() function

---
+++ square_draw.py
@@ -0,0 +1,14 @@
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    squareArtist = turtle.Turtle()
+    for i in range(4):
+        squareArtist.forward(100)
+        squareArtist.right(90)
+
+    window.exitonclick()
+
+draw_square()
