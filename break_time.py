4308fd8d243a4dee6d402014cfdbb91390a72acc
Alvin Kang <oneinallin1@gmail.com>
2017-08-05 19:06:25 -0700

    Print current time to show when program runs

--- break_time.py
+++ break_time.py
@@ -1,7 +1,9 @@
 import webbrowser
 import time
-
+print("This program started on: " + str(time.ctime()) + "\n")
 for i in range(0,3):
     time.sleep(5)
     webbrowser.open("https://youtu.be/jCHriYAr6Qw")
+    j = i+1
+    print("Loop count: " + str(j) + "\nCurrent time: " + str(time.ctime()))
 
